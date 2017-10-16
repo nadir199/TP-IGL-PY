bd5438b266ec2a29f84a4aebb737dde38862010b
Debbagh Nadir <nadir199@live.fr>
2017-10-16 01:12:36 +0100

    Ajouter la méthode trierVecteur et fixer le test sur cette méthode

---
+++ VectorHelper.py
@@ -0,0 +1,9 @@
+class VectorHelper:
+    @staticmethod
+    def trierVecteur(vecteur):
+        for j in range(1,len(vecteur)):
+            for i in range(j,len(vecteur)-1):
+                if(vecteur[j]>vecteur[j+1])
+                    tmp=vecteur[j]
+                    vecteur[j]=vecteur[j+1]
+                    vecteur[j+1]=tmp
--- test_vectorhelper.py
+++ test_vectorhelper.py
@@ -5,9 +5,9 @@ class VectorHelperTest(unittest.TestCase):
         test_case=[10,9,30,2,2,11,20,30,-5,0]
         trierVecteur(test_case)
         trie=true
-        for i in range(1,test_case.length-1):
+        for i in range(1,len(test_case)-1):
             if(test_case[i]>test_case[i+1]):
                 trie=false
                 break
         self.assertTrue(trie)
-        
+
