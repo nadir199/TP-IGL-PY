41adf3f5f493ad42b01cc0df639052e98b25e5dd
Hadjer <fh_benmeziane@esi.dz>
2017-10-17 19:19:03 +0000

    Ajout de la documentation pour appFormule

--- VectorHelper.py
+++ VectorHelper.py
@@ -2,9 +2,23 @@ class VectorHelper:
     def __init__(self):
         pass
 
-    @staticmethod
+     @staticmethod
     def appFormule(vecteur):
-        for i in range(0, len(vecteur)):
-            vecteur[i] = vecteur[i] * 2
+        """
+        Cette fonction permet d'appliquer la multiplication fois 2 à tous les éléments du vecteur en argument
+            :param vecteur: Un tableau d'entier sur lequel on applique la formule 
+            :return: Un tableau d'entier avec les valeurs * 2 
+            :rtype: list 
 
-        return vecteur 
+            :example: 
+
+            >>> appFormule([1,2])
+            [2,4]
+             
+
+        """
+        vec = []
+        for i in range(0, len(vecteur)):
+            vec[i] = vecteur[i] * 2
+
+        return vec
