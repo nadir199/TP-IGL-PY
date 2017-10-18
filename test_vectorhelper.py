d5b8c7de525c5d06e699173c22df172df4a93942
Hadjer <fh_benmeziane@esi.dz>
2017-10-18 18:59:28 +0000

    Ajout du test pour minMax

--- test_vectorhelper.py
+++ test_vectorhelper.py
@@ -8,10 +8,27 @@ class VectorHelperTest(unittest.TestCase):
         test_case=[10,9,30,2,2,11,20,30,-5,0]
         vec = VectorHelper.appFormule(test_case)
         
-        error = false
+        error = true
         for i in range(0,len(test_case)): 
         	if vec[i] != test_case[i] * 2:
-        		error = true
+        		error = false
+
+
+        self.assertTrue(error)
+        
+    def test_minMax(self):
+
+        test_case=[10,9,30,2,2,11,20,30,-5,0]
+        min = -5
+        max = 30
+        vec = VectorHelper.minMax(test_case)
+        
+        error = true
+        if vec[0] != min :
+        	error = false
+
+        if vec[1] != max :
+        	error = false
 
 
         self.assertTrue(error)
