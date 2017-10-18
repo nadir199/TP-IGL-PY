c51b0b1a0d9b4b9c276e6df5f8df846f8706e9c7
Hadjer <fh_benmeziane@esi.dz>
2017-10-18 18:56:00 +0000

    Ajout de la fonction minMax documenté

--- VectorHelper.py
+++ VectorHelper.py
@@ -22,3 +22,37 @@ class VectorHelper:
             vec[i] = vecteur[i] * 2
 
         return vec
+
+    @staticmethod
+    def minMax(vecteur):
+
+        """
+        Cette fonction permet de calculer simultanement le max et le min d'un vecteur 
+            :param vecteur: Un tableau d'entier 
+            :return: Un tableau d'entier [2] un pour le min et le deuxieme pour le max 
+            :rtype: list 
+
+            :example: 
+
+            >>> minMax([1,2,3,4])
+            [1,4]
+             
+
+        """
+
+        max = 0
+        min = 10000 
+
+        liste = []
+        for i in range(0,len(vecteur)):
+            if vecteur[i] > max : 
+                max = vecteur[i]
+
+            if vecteur[i] < min : 
+                min = vecteur[i]
+
+
+        liste[0] = min 
+        liste[1] = max 
+
+        return liste
