5ddcaabbbfc99d1e792c6c8bf17294afbe2330ac
Debbagh Nadir <nadir199@live.fr>
2017-10-20 09:05:10 +0100

    Ajout de la documentation pour trierVecteur

--- VectorHelper.py
+++ VectorHelper.py
@@ -1,9 +1,28 @@
+
+"""
+Ceci est un programme de manipulation de vecteurs
+"""
 class VectorHelper:
+    """
+        Cette classe est une classe utilitaire de manipulation des chaines de caractere
+    """
+
     def __init__(self):
         pass
 
     @staticmethod
     def trierVecteur(vecteur):
+        """
+        Trie un vecteur en ordre croissant
+
+        Args:
+            :type vecteur: int[]
+            :param vecteur: an array of numbers
+
+        Returns:
+            Le vecteur trie.
+
+        """
         for j in range(0,len(vecteur)):
             for i in range(0,len(vecteur)-1):
                 if vecteur[i]>vecteur[i+1]:
@@ -11,4 +30,3 @@ class VectorHelper:
                     vecteur[i]=vecteur[i+1]
                     vecteur[i+1]=tmp
         return vecteur
-
