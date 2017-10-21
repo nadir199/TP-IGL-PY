c46c435f16fd74e69c8cf977c5ce84835e820a6d
Hadjer <fh_benmeziane@esi.dz>
2017-10-21 22:07:28 +0000

    Fix code errors

--- VectorHelper.py
+++ VectorHelper.py
@@ -79,7 +79,7 @@ class VectorHelper:
         """
         vec = []
         for i in range(0, len(vecteur)):
-            vec[i] = vecteur[i] * 2
+            vec.append(vecteur[i] * 2)
 
         return vec
 
@@ -113,8 +113,8 @@ class VectorHelper:
                 min = vecteur[i]
 
 
-        liste[0] = min
-        liste[1] = max
+        liste.append( min)
+        liste.append(max)
 
         return liste
 
@@ -138,94 +138,13 @@ class VectorHelper:
 
         """
         vec = []
-        k = 0
+    
         for i in vect[::-1]:
-            vec[k] = i
-            k = k + 1
+            vec.append( i)
+      
         return vec
 
-    @staticmethod
-    def appFormule(vecteur):
-        """
-        
-        Cette fonction permet d'appliquer la multiplication fois 2 à tous les éléments du vecteur en argument
-        
-        :args: 
-        
-            :param vecteur: Un tableau d'entier sur lequel on applique la formule 
-            :return: Un tableau d'entier avec les valeurs * 2 
-            :rtype: list 
-
-        :example: >>> appFormule([1,2])
-                     [2,4]
-             
-
-        """
-        vec = []
-        for i in range(0, len(vecteur)):
-            vec[i] = vecteur[i] * 2
-
-        return vec
-
-    @staticmethod
-    def minMax(vecteur):
-
-        """
-        
-        Cette fonction permet de calculer simultanement le max et le min d'un vecteur d'entier :vecteur .
-        
-        args: 
-        
-            :param vecteur: Un tableau d'entier 
-            :return: Un tableau d'entier [2] un pour le min et le deuxieme pour le max 
-            :rtype: list 
-
-        :example:  >>> minMax([1,2,3,4])
-                       [1,4]
-
-        """
-
-        max = 0
-        min = 10000 
-
-        liste = []
-        for i in range(0,len(vecteur)):
-            if vecteur[i] > max : 
-                max = vecteur[i]
-
-            if vecteur[i] < min : 
-                min = vecteur[i]
-
-
-        liste[0] = min 
-        liste[1] = max 
-
-        return liste
-        
-    @staticmethod
-    def inverserVecteur(vect):
-        """
-
-        Cette fonction permet de retourner l'inverse du vecteur en entrée.
-
-        args:
-
-            :param vecteur: Un tableau d'entier
-            :return: Un tableau d'entier
-            :rtype: int[]
-
-        :example: >>> inverserVecteur([1,2,3,4])
-                     [4,3,2,1]
-
-
-        """
-        vec = []
-        k = 0
-        for i in vect[::-1]:
-            vec[k] = i
-            k = k + 1
-
-        return vec
+    
 
 class DifferentSizeVectorsException(Exception):
     def __init__(self):
