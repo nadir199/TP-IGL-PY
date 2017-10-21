ab1f572aaff3974091448ceb39bcf4539bebd7a1
Hadjer <fh_benmeziane@esi.dz>
2017-10-21 20:54:13 +0000

    Inistialiser le test unitaire

---
+++ test_vectorhelper.py
@@ -0,0 +1,7 @@
+import unittest
+from VectorHelper import VectorHelper
+
+class VectorHelperTest(unittest.TestCase):
+    """
+        Cette classe teste les méthodes de la classe 'VectorHelper'    
+    """
