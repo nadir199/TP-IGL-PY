a16f51815a01625d56a731d2c00c02c154bb19fa
Hadjer <fh_benmeziane@esi.dz>
2017-10-21 08:19:47 +0000

    Fix documentation

--- VectorHelper.py
+++ VectorHelper.py
@@ -1,19 +1,28 @@
+
 class VectorHelper:
+    
+    """
+        Cette classe est une classe utilitaire de manipulation des vecteurs d'entiers.
+        
+    """
+    
     def __init__(self):
         pass
 
      @staticmethod
     def appFormule(vecteur):
         """
+        
         Cette fonction permet d'appliquer la multiplication fois 2 à tous les éléments du vecteur en argument
+        
+        :args: 
+        
             :param vecteur: Un tableau d'entier sur lequel on applique la formule 
             :return: Un tableau d'entier avec les valeurs * 2 
             :rtype: list 
 
-            :example: 
-
-            >>> appFormule([1,2])
-            [2,4]
+        :example: >>> appFormule([1,2])
+                     [2,4]
              
 
         """
@@ -27,15 +36,17 @@ class VectorHelper:
     def minMax(vecteur):
 
         """
-        Cette fonction permet de calculer simultanement le max et le min d'un vecteur 
+        
+        Cette fonction permet de calculer simultanement le max et le min d'un vecteur d'entier :vecteur .
+        
+        args: 
+        
             :param vecteur: Un tableau d'entier 
             :return: Un tableau d'entier [2] un pour le min et le deuxieme pour le max 
             :rtype: list 
 
-            :example: 
-
-            >>> minMax([1,2,3,4])
-            [1,4]
+        :example:  >>> minMax([1,2,3,4])
+                       [1,4]
 
         """
 
@@ -58,20 +69,20 @@ class VectorHelper:
 
     
                   
-       @staticmethod 
+      @staticmethod 
       def inverserVecteur(vecteur):
         """
         
         Cette fonction permet de retourner l'inverse du vecteur en entrée. 
 
+        args: 
+        
             :param vecteur: Un tableau d'entier 
             :return: Un tableau d'entier
             :rtype: int[]
 
-            :example: 
-
-            >>> inverserVecteur([1,2,3,4])
-            [4,3,2,1]
+       :example: >>> inverserVecteur([1,2,3,4])
+                     [4,3,2,1]
              
 
         """
