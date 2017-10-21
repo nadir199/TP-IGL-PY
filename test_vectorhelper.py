4e44545fbe3e88b9ef5bebf558bb69018ce5cbad
Hadjer <fh_benmeziane@esi.dz>
2017-10-21 22:07:56 +0000

    Fix code errors

--- test_vectorhelper.py
+++ test_vectorhelper.py
@@ -57,10 +57,10 @@ class VectorHelperTest(unittest.TestCase):
         test_case=[10,9,30,2,2,11,20,30,-5,0]
         vec = VectorHelper.appFormule(test_case)
         
-        error = true
-        for i in range(0,len(test_case)): 
+        error = True
+        for i in range(len(test_case)): 
             if vec[i] != test_case[i] * 2:
-                error = false
+                error = False
                 break
 
 
@@ -76,12 +76,12 @@ class VectorHelperTest(unittest.TestCase):
         max = 30
         vec = VectorHelper.minMax(test_case)
         
-        error = true
+        error = True
         if vec[0] != min :
-            error = false
+            error = False
 
         if vec[1] != max :
-            error = false
+            error = False
 
 
         self.assertTrue(error)
@@ -96,12 +96,13 @@ class VectorHelperTest(unittest.TestCase):
             test_result = [0,-5,30,20,11,2,2,30,9,10]
             vec = VectorHelper.inverserVecteur(test_case)
 
-            error = false
+            error = True
             for i in range(len(test_case)):
                 if vec[i] != test_result[i]:
-                    error = true
+                    error = False
                     break
 
             self.assertTrue(error)
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
