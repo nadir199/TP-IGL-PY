9ac2ef0592b4f1cbcd4d77185637d68aa3ed2887
Hadjer <fh_benmeziane@esi.dz>
2017-10-21 20:53:00 +0000

    Initialiser la classe VectorHelper

---
+++ VectorHelper.py
@@ -0,0 +1,10 @@
+"""
+Ceci est un programme de manipulation de vecteurs
+"""
+class VectorHelper:
+    """
+        Cette classe est une classe utilitaire de manipulation des chaines de caractere
+    """
+
+    def __init__(self):
+        pass
